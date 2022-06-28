7d1bbadaa187fda1d424e336a2172c38e084e299
Tana <finaltruth.shintai@gmail.com>
2022-06-28 23:43:30 +0900

    戦闘表象IDの定義を専用クラスに集約。

---
+++ Src/BattleAssistGift/Keys/GiftID.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleAssistGift.Keys
+{
+    /// <summary>
+    /// 戦闘表象 ID を表します。
+    /// </summary>
+    public static class GiftID
+    {
+        /// <summary>「月光の輪」の戦闘表象 ID</summary>
+        public const int MoonlightRing = 197000;
+    }
+}
