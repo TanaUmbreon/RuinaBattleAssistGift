a84aa2cfcf6f59ca4115bf5eef17f21fb8596700
Tana <finaltruth.shintai@gmail.com>
2022-06-28 23:43:51 +0900

    戦闘表象の入手処理を実装。

--- Src/BattleAssistGift/BattleAssistGiftInitializer.cs
+++ Src/BattleAssistGift/BattleAssistGiftInitializer.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿#pragma warning disable IDE0051 // 使用されていないプライベート メンバーを削除する
+
+using System;
 using System.IO;
+using BattleAssistGift.Keys;
 using BattleAssistGift.Properties;
 using HarmonyLib;
-using BattleAssistGift.Keys;
 
 namespace BattleAssistGift
 {
@@ -12,6 +14,8 @@ namespace BattleAssistGift
     [Harmony]
     public class BattleAssistGiftInitializer : ModInitializer
     {
+        #region 初期化
+
         public override void OnInitializeMod()
         {
             try
@@ -37,18 +41,69 @@ namespace BattleAssistGift
             File.AppendAllText(ReferencePath.ModSettingsFile, Resources.ModSettings);
         }
 
+        #endregion
+
+        #region HarmonyPatch による割り込み処理 (戦闘表象の入手)
+
+        /// <summary>誰か一人でも戦闘表象を入手した事を示すフラグ</summary>
+        private static bool _hasGotGift = false;
+
+        /// <summary>
+        /// 戦闘表象の入手フラグを初期化します。
+        /// </summary>
+        /// <returns>本来呼び出されるメソッドも呼び出す場合は true、呼び出さない場合は false。</returns>
         [HarmonyPatch(typeof(LibraryModel), "LoadFromSaveData")]
+        [HarmonyPrefix]
+        private static bool LibraryModel_LoadFromSaveData_Prefix()
+        {
+            _hasGotGift = false;
+            return true;
+        }
+
+        /// <summary>
+        /// 指定司書に戦闘表象を入手させます。
+        /// </summary>
+        /// <param name="__instance">メソッドを呼び出したインスタンス。</param>
+        [HarmonyPatch(typeof(UnitDataModel), "LoadFromSaveData")]
         [HarmonyPostfix]
-        public static void LibraryModel_LoadFromSaveData_Postfix()
+        private static void UnitDataModel_LoadFromSaveData_Postfix(UnitDataModel __instance)
         {
             try
             {
-                Tools.ShowAlarmText(ExtraTextID.GotGifts);
+                if (!__instance.isSephirah) { return; }
+
+                GiftModel addedGift = __instance.giftInventory.AddGift(GiftID.MoonlightRing);
+                if (addedGift != null)
+                {
+                    _hasGotGift = true;
+                }
             }
             catch (Exception ex)
             {
                 Log.Instance.ErrorOnExceptionThrown(ex);
             }
         }
-	}
+
+        /// <summary>
+        /// 誰か一人でも戦闘表象を入手した場合、入手した事を知らせるアラート テキストを表示します。
+        /// </summary>
+        [HarmonyPatch(typeof(LibraryModel), "LoadFromSaveData")]
+        [HarmonyPostfix]
+        private static void LibraryModel_LoadFromSaveData_Postfix()
+        {
+            try
+            {
+                if (_hasGotGift)
+                {
+                    Tools.ShowAlarmText(ExtraTextID.GotGifts);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.ErrorOnExceptionThrown(ex);
+            }
+        }
+
+        #endregion
+    }
 }
